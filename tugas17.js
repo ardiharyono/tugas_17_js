e660b367403cc2482f9734674abfe060ac6f99a6
Ardi Haryono <ardiharyono26@gmail.com>
2021-10-16 17:32:11 +0700

    Tugas 17 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas17.js
@@ -0,0 +1,17 @@
+function pertama() {
+  var satu = "Saya beLajar bahaSa peMrograman deNgan khUsuk";
+  console.log(satu.toLowerCase());
+  console.log(satu.toUpperCase());
+  var dua = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual.";
+  console.log(dua.toLowerCase());
+  console.log(dua.toUpperCase());
+}
+
+pertama()
+console.log("====================");
+function kedua() {
+  var ascii = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+  console.log(ascii.charCodeAt(0) + "-" + ascii.charCodeAt(1) + "-" + ascii.charCodeAt(2) + "-" + ascii.charCodeAt(3) + "-" + ascii.charCodeAt(4) + "-" + ascii.charCodeAt(5));
+}
+
+kedua()
